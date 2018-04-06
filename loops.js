242649e923cb608e44752e2c43dade25a54ba7e3
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-04-06 02:42:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,7 +22,9 @@ function whileLoop(n) {
 function maybeTrue() {
   return Math.random() >= 0.5
 }
+
 function doWhileLoop(array) {
  do {
-  while (maybeTrue());
+ }
+  while (array.length > 0 && maybeTrue());
 }
