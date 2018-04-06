c0dd5049174b2e97187cd0470052f212028a765b
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-04-06 02:12:25 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop(array) {
+  for (let i = 0; i<25; i++) {
+    if (i = 1) {
+      var string = "I am 1 strange loop."
+      array.push(string)
+    }
+    else {
+      var strings= "I am ${i} strange loops."
+      array.push(strings)
+    }
+  }
+  return array
+}
